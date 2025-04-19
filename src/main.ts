ab1e811140abd4d8af1dca03eead75cf36ac6843
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-04-19 12:12:59 +0200

    feat: add opensearch + public url

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="${import.meta.env.VITE_PUBLIC_URL}?q=%s"
             readonly 
           />
           <button class="copy-button">
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
+import { resolve, join } from "node:path";
+import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import transformPlugin from "vite-plugin-transform";
 
-export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      transformPlugin({
+        tStart: "%{",
+        tEnd: "}%",
+        replaceFiles: [resolve(join(__dirname, "./public/opensearch.xml"))],
+        replace: {
+          VITE_PUBLIC_URL: env.VITE_PUBLIC_URL,
+        },
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+      }),
+    ],
+    server: {
+      port: parseInt(env.PORT),
+    }
+  };
 });
